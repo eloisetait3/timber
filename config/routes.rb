0b5e5a9c4c71738563837f27ef339c684c3df460
Megan Thresh <meganthresh96@gmail.com>
2020-05-18 17:44:16 +0100

    comitting the routes and initialized the controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,27 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  #  root: home <DONE>
+  resources :houses do 
+    resources :bookings, only: [:create, :new]
+  end
+
+  resources :bookings, only: [:destroy]
+  resources :users, only: [:show]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+
+
+
+
+  #  root: home <DONE> 
   #  index for account
-  #  show_search_result (collection)
+  #  show_search_result (collection) - 
   #  show_individual_treehouse (member)
   #  new_treehouse (form) + create
-end
+
+    # resources :houses do
+  #   collection do
+  #     get 'search'
+  #   end
+  # end
