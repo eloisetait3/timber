c9097d7dd4210d6a3c101b01706dd66a1bab1eac
Eloise Tait <eloisetait@yahoo.com>
2020-05-18 16:03:03 +0100

    Models created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #  root: home <DONE>
+  #  index for account
+  #  show_search_result (collection)
+  #  show_individual_treehouse (member)
+  #  new_treehouse (form) + create
 end
