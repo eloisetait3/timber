4bd5d50aa278d3482cab5e8cb45d92b684411ce0
Eloise Tait <eloisetait@yahoo.com>
2020-05-19 17:26:52 +0100

    Create a booking view + controller (also added guests column to table)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,23 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @house = House.find(params[:house_id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.house = House.find(params[:house_id].to_i)
+    @booking.user = current_user
+    if @booking.save
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:checkin, :checkout, :guests, :house_id)
+  end
 end
