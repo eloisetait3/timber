4badecacdff42ae76c8293b854021cc4d59bd6c7
Akane Hirabayashi <akanehirabayashi@gmail.com>
2020-05-19 12:59:47 +0100

    edited house controller

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,2 +1,24 @@
 class HousesController < ApplicationController
+  before_action :set_house, only: [:show, :edit, :update]
+
+  def show
+    @booking = Booking.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @house.update(house_params)
+      redirect_to house_path(@house)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_house
+    @house = House.find(params[:id])
+  end
 end
