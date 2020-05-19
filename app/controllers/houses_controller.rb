9eedf7d88991d9b9a332f20b02df7ecf7f189349
PasqualeTroise <troisepasquale1990@gmail.com>
2020-05-19 14:13:22 +0100

    Added houses#new, houses#create and the houses/new.html.erb page

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,2 +1,23 @@
 class HousesController < ApplicationController
+  def new
+    @house = House.new
+  end
+  
+  def create
+    @house = House.new(houses_params)
+    @user = current_user
+    @house.user = @user
+    @house.save
+    if @house.save
+      redirect_to house_path(@house)
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def houses_params
+    params.require(:house).permit(:name, :location, :description, :price)
+  end
 end
