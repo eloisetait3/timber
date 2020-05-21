d921d65b9713d5124bcc5a57f1471ebeff91cf53
PasqualeTroise <troisepasquale1990@gmail.com>
2020-05-21 15:45:56 +0100

    Implemented a no-possibility-booking-when-it-already-exists feature

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,15 @@
 class Booking < ApplicationRecord
   belongs_to :house
   belongs_to :user
+
+  validate :check_availability
+
+  def check_availability
+    query = "house_id= ? AND checkin= ? AND checkout= ?", self.house.id, "%#{self.checkin}%", "%#{self.checkout}%"
+    # raise
+    if Booking.where(query).exists?
+      error_message = "This treehouse is not available for the given date"
+      errors.add(:checkin, error_message)
+    end
+  end
 end
